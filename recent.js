6a2652e516bd8ac6ecc4afb68b3dd385bfa81d6e
Ishwarya-24 <111902780+Ishwarya-24@users.noreply.github.com>
2023-03-01 11:46:42 +0400

    second commit

--- recent.js
+++ recent.js
@@ -20,3 +20,196 @@ function setImg(){
 	return slider_img.setAttribute('src', "images/"+images[i]);
 	
 }
+
+// Services js
+
+// 'js/mian.js'
+
+var slider_img = document.querySelector('.slider-img');
+var images = ['turf1.jpg', 'turf2.jpg'];
+var i = 0;
+
+function prev(){
+	if(i <= 0) i = images.length;	
+	i--;
+	return setImg();			 
+}
+
+function next(){
+	if(i >= images.length-1) i = -1;
+	i++;
+	return setImg();			 
+}
+
+function setImg(){
+	return slider_img.setAttribute('src', "images/"+images[i]);
+	
+}
+// Services js
+
+// 'js/mian.js'
+
+var slider_img = document.querySelector('.slider-img');
+var images = ['pumpingservices1.jpg', 'pumpingservices2.jpg', 'pumpingservices2.jpg'];
+var i = 0;
+
+function prev(){
+	if(i <= 0) i = images.length;	
+	i--;
+	return setImg();			 
+}
+
+function next(){
+	if(i >= images.length-1) i = -1;
+	i++;
+	return setImg();			 
+}
+
+function setImg(){
+	return slider_img.setAttribute('src', "images/"+images[i]);
+	
+}
+// Services js
+
+// 'js/mian.js'
+
+var slider_img = document.querySelector('.slider-img');
+var images = ['lake1.jpg', 'lake2.jpg'];
+var i = 0;
+
+function prev(){
+	if(i <= 0) i = images.length;	
+	i--;
+	return setImg();			 
+}
+
+function next(){
+	if(i >= images.length-1) i = -1;
+	i++;
+	return setImg();			 
+}
+
+function setImg(){
+	return slider_img.setAttribute('src', "images/"+images[i]);
+	
+}
+// Services js
+
+// 'js/mian.js'
+
+var slider_img = document.querySelector('.slider-img');
+var images = ['environment1.jpg', 'environment2.jpg', 'environment3.jpg', 'environment4.jpg','environment5.jpg','environment6.jpg'];
+var i = 0;
+
+function prev(){
+	if(i <= 0) i = images.length;	
+	i--;
+	return setImg();			 
+}
+
+function next(){
+	if(i >= images.length-1) i = -1;
+	i++;
+	return setImg();			 
+}
+
+function setImg(){
+	return slider_img.setAttribute('src', "images/"+images[i]);
+	
+}
+// Services js
+
+// 'js/mian.js'
+
+var slider_img = document.querySelector('.slider-img');
+var images = ['fogging1.jpg', 'fogging2.jpg'];
+var i = 0;
+
+function prev(){
+	if(i <= 0) i = images.length;	
+	i--;
+	return setImg();			 
+}
+
+function next(){
+	if(i >= images.length-1) i = -1;
+	i++;
+	return setImg();			 
+}
+
+function setImg(){
+	return slider_img.setAttribute('src', "images/"+images[i]);
+	
+}
+// Services js
+
+// 'js/mian.js'
+
+var slider_img = document.querySelector('.slider-img');
+var images = ['aeration1.jpg', 'aeration2.jpg', 'aeration3.jpg', 'aeration4.jpg'];
+var i = 0;
+
+function prev(){
+	if(i <= 0) i = images.length;	
+	i--;
+	return setImg();			 
+}
+
+function next(){
+	if(i >= images.length-1) i = -1;
+	i++;
+	return setImg();			 
+}
+
+function setImg(){
+	return slider_img.setAttribute('src', "images/"+images[i]);
+	
+}
+// Services js
+
+// 'js/mian.js'
+
+var slider_img = document.querySelector('.slider-img');
+var images = ['agriculture1.jpg', 'agriculture2.jpg', 'agriculture3.jpg', 'agriculture4.jpg'];
+var i = 0;
+
+function prev(){
+	if(i <= 0) i = images.length;	
+	i--;
+	return setImg();			 
+}
+
+function next(){
+	if(i >= images.length-1) i = -1;
+	i++;
+	return setImg();			 
+}
+
+function setImg(){
+	return slider_img.setAttribute('src', "images/"+images[i]);
+	
+}
+// Services js
+
+// 'js/mian.js'
+
+var slider_img = document.querySelector('.slider-img');
+var images = ['floating1.jpg', 'floating2.jpg'];
+var i = 0;
+
+function prev(){
+	if(i <= 0) i = images.length;	
+	i--;
+	return setImg();			 
+}
+
+function next(){
+	if(i >= images.length-1) i = -1;
+	i++;
+	return setImg();			 
+}
+
+function setImg(){
+	return slider_img.setAttribute('src', "images/"+images[i]);
+	
+}
